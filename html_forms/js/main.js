16e56bd69513c5ed28a3337289e5a07933e4ca2c
סלים סבייח <sbsaleem76@gmail.com>
2019-12-22 16:42:15 +0200

    files in

---
+++ html_forms/js/main.js
@@ -0,0 +1,101 @@
+(function($) {
+
+  $('#state').parent().append('<ul class="list-item" id="newstate"></ul>');
+  $('#state option').each(function(){
+      $('#newstate').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
+  });
+  $('#state').remove();
+  $('#newstate').attr('id', 'state');
+  $('#state li').first().addClass('init');
+  $("#state").on("click", ".init", function() {
+      $(this).closest("#state").children('li:not(.init)').toggle();
+  });
+  
+  var allOptions = $("#state").children('li:not(.init)');
+  $("#state").on("click", "li:not(.init)", function() {
+      allOptions.removeClass('selected');
+      $(this).addClass('selected');
+      $("#state").children('.init').html($(this).html());
+      allOptions.toggle();
+  });
+
+  var marginSlider = document.getElementById('slider-margin');
+  if (marginSlider != undefined) {
+      noUiSlider.create(marginSlider, {
+            start: [500],
+            step: 10,
+            connect: [true, false],
+            tooltips: [true],
+            range: {
+                'min': 0,
+                'max': 1000
+            },
+            format: wNumb({
+                decimals: 0,
+                thousand: ',',
+                prefix: '$ ',
+            })
+    });
+  }
+  $('#reset').on('click', function(){
+      $('#register-form').reset();
+  });
+
+  $('#register-form').validate({
+    rules : {
+        victim_name : {
+            required: true,
+        },
+        age : {
+            required: true,
+        },
+        contactno : {
+            required: true,
+        },
+        address : {
+            required: true
+        },
+        email : {
+            required: true,
+            email : true
+        },
+        description : {
+            required: true,
+        },
+        criminalName : {
+            required: true,
+        },
+        rfName : {
+            required: true,
+        },
+        victim_number : {
+            required: true,
+        },
+        nGOName : {
+            required: true,
+        },
+        filingPersonName : {
+            required: true,
+        },
+        experience : {
+            required: true,
+        }
+    },
+    onfocusout: function(element) {
+        $(element).valid();
+    },
+});
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "",
+        remote: "",
+        email: "",
+        url: "",
+        date: "",
+        dateISO: "",
+        number: "",
+        digits: "",
+        creditcard: "",
+        equalTo: ""
+    });
+})(jQuery);
